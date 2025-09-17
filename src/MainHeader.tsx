b54fbdd62d2d1140225b25dbe7fe01924b9519bb
SerKasai <serjtheignis92@gmail.com>
2025-09-17 19:35:49 +0200

    intro progetto

---
+++ src/MainHeader.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const links = [
+  {
+    text: "Characters",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Comics",
+    href: "#",
+    active: true,
+  },
+  {
+    text: "Movies",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "TV",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Games",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Collectibles",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Videos",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Fans",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "News",
+    href: "#",
+    active: false,
+  },
+  {
+    text: "Shop",
+    href: "#",
+    active: false,
+  },
+];
