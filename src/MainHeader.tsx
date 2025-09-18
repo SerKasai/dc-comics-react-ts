3c790b017416c4eeea0ae793411db5c93474193b
SerKasai <serjtheignis92@gmail.com>
2025-09-18 12:39:21 +0200

    creetae header

--- src/MainHeader.tsx
+++ src/MainHeader.tsx
@@ -1,54 +1,82 @@
 import { useState } from "react";
+import "./MainHeader.css";
 
-const links = [
+const arrayLinks = [
   {
+    id: 0,
     text: "Characters",
     href: "#",
     active: false,
   },
   {
+    id: 1,
     text: "Comics",
     href: "#",
     active: true,
   },
   {
+    id: 2,
     text: "Movies",
     href: "#",
     active: false,
   },
   {
+    id: 3,
     text: "TV",
     href: "#",
     active: false,
   },
   {
+    id: 4,
     text: "Games",
     href: "#",
     active: false,
   },
   {
+    id: 5,
     text: "Collectibles",
     href: "#",
     active: false,
   },
   {
+    id: 6,
     text: "Videos",
     href: "#",
     active: false,
   },
   {
+    id: 7,
     text: "Fans",
     href: "#",
     active: false,
   },
   {
+    id: 8,
     text: "News",
     href: "#",
     active: false,
   },
   {
+    id: 9,
     text: "Shop",
     href: "#",
     active: false,
   },
 ];
+
+function MainHeader() {
+  return (
+    <header className="text-black flex w-full justify-evenly">
+      <img src="/src/assets/dc-logo.png" className="cursor-pointer" />
+      <ul className="flex flex-wrap gap-6 uppercase text-xl items-center justify-center content-center bebas-neue-regular">
+        {arrayLinks.map((link) => (
+          <li key={link.id} className="hover:text-blue-600 cursor-pointer">
+            {link.text}
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+}
+
+export default MainHeader;
